494e2aabee44b30da91cd37cf6e462dc937540df
beatrizslcosimatti <beatrizslcosimatti@gmail.com>
2025-05-06 19:08:59 -0300

    adicionando codigo metros para pes

---
+++ metros_para_pes.py
@@ -0,0 +1,10 @@
+#converter medidas de metros para pes
+
+#ler valor em metros
+metros = float(input("insira valor em metros: "))
+ 
+#converter valor para pes
+pes = metros*3.28084 
+
+#imprimir valor em pes
+print("o valor em pés é:", pes)
