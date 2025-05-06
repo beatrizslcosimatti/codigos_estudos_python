735ce4faef389791427f7f2215a13b5f8ec24c6f
beatrizslcosimatti <beatrizslcosimatti@gmail.com>
2025-05-06 19:13:30 -0300

    adicionando codigo graus para kelvin e farenheit

---
+++ graus_para_K_e_F.py
@@ -0,0 +1,16 @@
+#converter graus para kelvin e farenheit 
+
+#ler valor em graus
+graus = float(input("insira a temperatura em graus: "))
+ 
+#converter graus para kelvin
+kelvin = graus + 273.15 
+
+#imprimir valor em kelvin
+print("a temperatura em Kelvin é:", kelvin)
+
+#converter graus para farenheit
+F = graus*1.8 + 32 
+
+#imprimir valor em farenheit
+print("a temperatura em Farenheit é:",F)
